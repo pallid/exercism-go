b3fd3cf00f2a2ee176cd5a7812f5e58401f9cb76
vasily popov <1005220@mail.ru>
2019-04-08 17:48:16 +0300

    acronym - go

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,15 +1,26 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-// Abbreviate should have a comment documenting it.
+import (
+	"strings"
+)
+
+var replacer = strings.NewReplacer(",", "", "'", "", "-", " ")
+
+// Convert a phrase to its acronym
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	res := ""
+	str := replacer.Replace(s)
+
+	words := strings.Split(str, " ")
+	for _, word := range words {
+		if len(word) != 0 {
+
+			res = res + string([]rune(word)[0])
+		}
+	}
+
+	res = strings.ToUpper(res)
+
+	return res
 }
